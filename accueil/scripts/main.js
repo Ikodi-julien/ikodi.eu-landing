8987f2a5d298d3c8a265ba8940549aac13a4ff30
ikodi-julien <jupellin39@gmail.com>
2020-12-26 18:48:20 +0100

    Début de refonte de la page d'accueil d'IKODI

--- accueil/scripts/main.js
+++ accueil/scripts/main.js
@@ -1,75 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-  /* --- SHOW MENU --- */
-  function showMenu(hamburgerId, navId) {
-    const hamburger = document.getElementById(hamburgerId),
-      nav = document.getElementById(navId)
-    if (hamburger && nav) {
-      hamburger.addEventListener('click', () => {
-        nav.classList.toggle('show')
-      })
-    }
-  }
-  showMenu('nav-hamburger', 'nav-menu')
+document.addEventListener("DOMContentLoaded", () => {
+  const loader = document.getElementById("loader");
+  loader.classList.add("hide");
 
-  /* --- BURGER ANIMATION --- */
-  const animBurger = document.getElementById('nav-hamburger')
-  let menuOpen = false
-  animBurger.addEventListener('click', () => {
-    if (!menuOpen) {
-      animBurger.classList.add('open')
-      menuOpen = true
-    } else {
-      animBurger.classList.remove('open')
-      menuOpen = false
-    }
-  })
+  // document.addEventListener("click", () => {
+  //   loader.classList.toggle("hide");
+  // });
 
-  /* --- GSAP --- */
-  //Navbar
-  gsap.from('.nav__title', {
-    opacity: 0,
-    duration: 3,
-    delay: 0.8,
-    y: 30,
-    ease: 'expo.out',
-  })
-  gsap.from('.nav__hamburger', {
-    opacity: 0,
-    duration: 3,
-    delay: 1.1,
-    y: 30,
-    ease: 'expo.out',
-  })
-  gsap.from('.nav__link', {
-    opacity: 0,
-    duration: 3,
-    delay: 1,
-    y: 35,
-    ease: 'expo.out',
-    stagger: 0.2,
-  })
-  //Text
-  gsap.from('.landing__title', {
-    opacity: 0,
-    duration: 3,
-    delay: 1.6,
-    y: 35,
-    ease: 'expo.out',
-  })
-  gsap.from('.landing__subtitle', {
-    opacity: 0,
-    duration: 3,
-    delay: 1.4,
-    y: 35,
-    ease: 'expo.out',
-  })
-
-  //Scroll
-  gsap.from('.landing__scroll', {
-    opacity: 0,
-    duration: 3,
-    delay: 1.8,
-    y: 35,
-    ease: 'expo.out',
-  })
-})
+  /*--------------------------------------------------*/
+  /*----------- HAMBURGER ----------------------------*/
+  /*--------------------------------------------------*/
+  const hamburger = document.getElementById("nav-hamburger");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open");
+  });
+});
